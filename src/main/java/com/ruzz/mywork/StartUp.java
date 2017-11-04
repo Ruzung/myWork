9a9f4a9a309891ab86824cc883dbe86bcdccb2c6
茹宗智 <ruzongzhi@bonc.com.cn>
2017-11-04 16:18:37 +0800

    application测试

--- src/main/java/com/ruzz/mywork/StartUp.java
+++ src/main/java/com/ruzz/mywork/StartUp.java
@@ -2,16 +2,15 @@ package com.ruzz.mywork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Hello world!
  *
  */
-//@SpringBootApplication
-@Configuration
-public class StartUp extends WebMvcConfigurerAdapter
+@SpringBootApplication
+//@Configuration
+public class StartUp
 {
 
         public static void main(String[] args) throws Exception
--- src/main/java/com/ruzz/mywork/controller/WebController.java
+++ src/main/java/com/ruzz/mywork/controller/WebController.java
@@ -1,18 +1,25 @@
 package com.ruzz.mywork.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ruzz.mywork.cof.Mycof;
+
 @Controller
-@RequestMapping("/myWork")
+@RequestMapping("/mywork")
 public class WebController {
+    
+    @Autowired
+    private Mycof cof;
     
     @ResponseBody
     @RequestMapping(value={"/hello"},method=RequestMethod.GET)
     public String webHelloWork() {
         
+        System.out.println(cof.getName());
         System.out.println("hello world!");
         return "hello world";
     }
